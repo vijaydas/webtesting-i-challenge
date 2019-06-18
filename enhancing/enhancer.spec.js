84f90c1784d9b4380d94243f2f97c9d9d1a1b748
Vijay Das <vijaytdas@gmail.com>
2019-06-18 17:50:30 -0500

    repair function and tests written

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
-const enhancer = require('./enhancer.js');
+const { repair } = require("./enhancer.js");
 // test away!
+
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("restores durability to 100", () => {
+      expect(repair({ durability: 100 }).durability).toBe(100);
+    });
+    it("restores durability to 100", () => {
+      expect(repair({ durability: 50 }).durability).toBe(100);
+    });
+    it("restores durability to 100", () => {
+      expect(repair({ durability: 320 }).durability).toBe(100);
+    });
+  });
+});
